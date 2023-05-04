59ad81b779a28f607246d4c036fb8c704a816721
Radek Pietrzak <radekpietrzak102@gmail.com>
2023-05-04 12:57:03 +0200

    [#ABM-23] JsonSerialize responses

--- product-service/src/main/java/com/productservice/api/response/AuthorResponse.java
+++ product-service/src/main/java/com/productservice/api/response/AuthorResponse.java
@@ -1,5 +1,6 @@
 package com.productservice.api.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -7,6 +8,7 @@ import lombok.EqualsAndHashCode;
 @Builder
 @AllArgsConstructor
 @EqualsAndHashCode
+@JsonSerialize
 public class AuthorResponse {
     private String name;
     private String description;
--- product-service/src/main/java/com/productservice/api/response/PublisherResponse.java
+++ product-service/src/main/java/com/productservice/api/response/PublisherResponse.java
@@ -1,10 +1,12 @@
 package com.productservice.api.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
 @AllArgsConstructor
 @EqualsAndHashCode
+@JsonSerialize
 public class PublisherResponse {
     private String publisherName;
     private String description;
