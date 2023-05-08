846eb20e7025c040ceb41f4e52635edd8f8e21ed
Radek Pietrzak <radekpietrzak102@gmail.com>
2023-05-08 12:18:47 +0200

    [#ABM-23] remove MongoDBConfig.java

--- product-service/src/main/java/com/productservice/repository/BookRepository.java
+++ product-service/src/main/java/com/productservice/repository/BookRepository.java
@@ -1,11 +1,9 @@
 package com.productservice.repository;
 
-import com.productservice.config.MongoDBConfig;
 import com.productservice.entity.Book;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -43,29 +41,7 @@ public interface BookRepository extends MongoRepository<Book, String> {
                 ]
             }""";
 
-    @org.springframework.data.mongodb.repository.Query(DEFAULT_QUERY)
+    @Query(DEFAULT_QUERY)
     List<Book> findBySearchTermAndPageRequest(String search, PageRequest pageRequest);
 
-
-    // TODO move this method to separate class
-    default List<Book> findWithSearch(String search, PageRequest pageRequest) {
-        if (search == null) {
-            search = "";
-        }
-        Query query = new Query();
-        Criteria criteria = new Criteria().andOperator(
-                Criteria.where("deletedDate").is(null),
-                new Criteria().orOperator(
-                        Criteria.where("title").regex(search, "i"),
-                        Criteria.where("ISBN").regex(search, "i"),
-                        Criteria.where("authors.name").regex(search, "i"),
-                        Criteria.where("description").regex(search, "i"),
-                        Criteria.where("categories.name").regex(search, "i"),
-                        Criteria.where("publisher.name").regex(search, "i")
-                )
-        );
-        query.addCriteria(criteria);
-        MongoDBConfig mongoDBConfig = new MongoDBConfig();
-        return mongoDBConfig.mongoTemplate().find(query.with(pageRequest), Book.class);
-    }
 }
