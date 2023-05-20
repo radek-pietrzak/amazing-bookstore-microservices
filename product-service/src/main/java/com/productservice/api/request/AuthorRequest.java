20415d4f31ae93ec99ef91147ea09ad65060cc23
Radek Pietrzak <radekpietrzak102@gmail.com>
2023-05-20 10:32:39 +0200

    [#ABM-20] editBook request example

--- product-service/src/main/java/com/productservice/api/request/AuthorRequest.java
+++ product-service/src/main/java/com/productservice/api/request/AuthorRequest.java
@@ -1,30 +1,24 @@
 package com.productservice.api.request;
 
 import com.productservice.validation.ValidationErrors;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class AuthorRequest {
 
     @NotNull(message = ValidationErrors.AUTHOR_NAME_NULL)
     @Size(min = 1, max = 255, message = ValidationErrors.AUTHOR_NAME_LENGTH)
+    @Schema(example = "J.D.")
     private String name;
+
     @Size(min = 1, max = 1000, message = ValidationErrors.AUTHOR_DESCRIPTION_LENGTH)
+    @Schema(example = "Salinger")
     private String description;
-
-    public AuthorRequest() {
-    }
-
-    public AuthorRequest(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
