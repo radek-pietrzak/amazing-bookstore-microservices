c87b0e5099b60d03a220aae94913b8995bacbf92
Radek Pietrzak <radekpietrzak102@gmail.com>
2023-05-21 08:58:27 +0200

    [#ABM-20] response schema

--- product-service/src/main/java/com/productservice/api/response/PublisherResponse.java
+++ product-service/src/main/java/com/productservice/api/response/PublisherResponse.java
@@ -1,5 +1,6 @@
 package com.productservice.api.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -8,6 +9,9 @@ import lombok.Getter;
 @EqualsAndHashCode
 @Getter
 public class PublisherResponse {
+    @Schema(description = "Publisher name", example = "Little, Brown and Company")
     private String publisherName;
+
+    @Schema(description = "Publisher description", example = "Boston")
     private String description;
 }
