2839a40caed7ed12735493f0607f876f3d3de3b8
Radek Pietrzak <radekpietrzak102@gmail.com>
2023-06-30 09:03:27 +0200

    [#ABM-32] create product-autofill-service

---
+++ product-autofill-service/src/main/java/com/productautofillservice/ProductAutofillServiceApplication.java
@@ -0,0 +1,13 @@
+package com.productautofillservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAutofillServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductAutofillServiceApplication.class, args);
+    }
+
+}
