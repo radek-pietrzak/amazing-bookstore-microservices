8494d0884a27de08e27e385043e8a6a3268eac76
Radek Pietrzak <radekpietrzak102@gmail.com>
2025-09-18 10:09:10 +0200

    product-service api endpoints update

--- product-service/src/main/java/pl/radek/productservice/api/controller/API.java
+++ product-service/src/main/java/pl/radek/productservice/api/controller/API.java
@@ -2,16 +2,17 @@ package pl.radek.productservice.api.controller;
 
 public interface API {
 
+    String API = "/api/product";
     String ID = "{id}";
-    String BOOK_GET = "/product-service/book/";
+    String BOOK_GET = API + "/book/";
     String BOOK_GET_ID = BOOK_GET + ID;
-    String BOOK_SAVE = "/product-service/book/save";
-    String BOOK_EDIT = "/product-service/book/edit/";
+    String BOOK_SAVE = API + "/book/save";
+    String BOOK_EDIT = API + "/book/edit/";
     String BOOK_EDIT_ID = BOOK_EDIT + ID;
-    String BOOK_DELETE = "/product-service/book/delete/";
+    String BOOK_DELETE = API + "/book/delete/";
     String BOOK_DELETE_ID = BOOK_DELETE + ID;
-    String BOOK_LIST = "/product-service/book-list";
-    String ISBN_LIST = "/product-service/isbn-list";
-    String BOOK_LIST_SAVE = "/product-service/book-list/save";
+    String BOOK_LIST = API + "/book-list";
+    String ISBN_LIST = API + "/isbn-list";
+    String BOOK_LIST_SAVE = API + "/book-list/save";
 
 }
